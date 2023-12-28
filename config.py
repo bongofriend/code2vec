cb8c1588bdc86a3354eb7a902f0a308e95f075ae
bongofriend <memiaslan1905@gmail.com>
2023-12-28 21:24:42 +0100

    Adjust config

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ class Config:
         return parser
 
     def set_defaults(self):
-        self.NUM_TRAIN_EPOCHS = 20
+        self.NUM_TRAIN_EPOCHS = 50
         self.SAVE_EVERY_EPOCHS = 1
         self.TRAIN_BATCH_SIZE = 1024
         self.TEST_BATCH_SIZE = self.TRAIN_BATCH_SIZE
@@ -54,7 +54,7 @@ class Config:
         self.READER_NUM_PARALLEL_BATCHES = 6  # cpu cores [for tf.contrib.data.map_and_batch() in the reader]
         self.SHUFFLE_BUFFER_SIZE = 10000
         self.CSV_BUFFER_SIZE = 100 * 1024 * 1024  # 100 MB
-        self.MAX_TO_KEEP = 10
+        self.MAX_TO_KEEP = 50
 
         # model hyper-params
         self.MAX_CONTEXTS = 200
